f75a988b35905b9c66105faeb578034e2f22e9ab
TaikiFnit <taikifnitspg@gmail.com>
2019-12-07 03:04:16 +0900

    fix start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-node bedrock-manager/src/input_piper.js | LD_LIBRARY_PATH=. ./bedrock_server | bedrock-manager/src/output_piper.js
+node bedrock-manager/src/input_piper.js | LD_LIBRARY_PATH=. ./bedrock_server | node bedrock-manager/src/output_piper.js
