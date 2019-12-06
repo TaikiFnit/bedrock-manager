8cdd5e3fab33aadb76afbeaedb14dd7997e437a5
TaikiFnit <taikifnitspg@gmail.com>
2019-12-07 03:20:05 +0900

    とりあえず, server logをdiscord webhookに

--- src/output_manager.js
+++ src/output_manager.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const axios = require('axios');
-const log = require('./lib/logSubscribeManager');
 
 require('dotenv').config();
-const manager_port = process.env.output_piper_port;
+const manager_port = process.env.manager_port;
+const webhook_url = process.env.discord_webhook;
 
 /* Express */
 const app = express();
@@ -11,17 +11,22 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const logSubscriber = [];
+
 app.post('/log', async (req, res) => {
-    console.log(req.body.log);
-    log.logSubscriber.forEach(subscriber => subscriber(req.body.log));
+    logSubscriber.forEach(subscriber => subscriber(req.body.log));
     return res.send('ok');
 });
 
 app.listen(manager_port);
 
 /* web hook sender */
-log.subscribe((log) => {
+logSubscriber.push((log) => {
     console.log('subscriber');
     console.log(log);
+    const data = {
+        content: log
+    };
+    axios.post(webhook_url, data)
 });
 
