b02c1f48fbe785fd36a534d15d84ac02f5860520
TaikiFnit <taikifnitspg@gmail.com>
2020-08-25 01:22:09 +0900

    bakcupの保存期間をenvファイルで指定できるように

--- src/backup_manager.js
+++ src/backup_manager.js
@@ -6,24 +6,31 @@ const webhook_url = process.env.discord_webhook;
 do_backup();
 
 setInterval(() => {
-    console.log('interval 1 day');
-    do_backup();
+  console.log('interval 1 day');
+  do_backup();
 }, 1000 * 60 * 60 * 24);
 
 function do_backup() {
-    exec("zip -r ../backups/minecraft_`date +%Y%m%d-%H%M%S`.zip ../worlds/", (err, stdout, stderr) => {
-        if (err) { console.log(err); }
-        console.log(stdout);
-    });
+  exec("zip -r ../backups/minecraft_`date +%Y%m%d-%H%M%S`.zip ../worlds/", (err, stdout, stderr) => {
+  if (err) { console.log(err); }
+    console.log(stdout);
+  });
 
-    exec("find ../backups/ -mtime +7 -exec rm -f {} \\;", (err, stdout, stderr) => {
-	if (err) { console.log(err); }
-	console.log(stdout);
+  console.log('[INFO] Following command will be executed if backup_retention_period > 0.');
+  console.log(`find ../backups/ -mtime +${process.env.backup_retention_period} -exec rm -f {} \\;`);
+  if(process.env.backup_retention_period > 0) {
+    exec(`find ../backups/ -mtime +${process.env.backup_retention_period} -exec rm -f {} \\;`, (err, stdout, stderr) => {
+    if (err) { console.log(err); }
+      console.log(stdout);
     });
+  }
 
+  if(webhook_url.match(/http/)) {
     const data = {
-        content: 'Worlds is backed up.'
+      content: 'Worlds is backed up.'
     };
     axios.post(webhook_url, data);
-
+  } else {
+    console.log('[INFO] webhook url is not set. it will not notify.');
+  }
 }
